bdff230cbd19e794b42fbbfe0d7e64da4d509eea
zhanghaifeng <zhanghaifeng@xiaomi.com>
2019-07-16 17:23:37 +0800

    add tokens

--- cover.sh
+++ cover.sh
@@ -3,6 +3,7 @@
 # wake up heroku!
 #curl http://demo.opencov.com || true
 
+export COVERALLS_REPO_TOKEN=UUhvWDVPUjVXZkZyRUJSNFc4cFkzTEpKQWZaVHVHeC9OWEVrV1ZRMQ==
 MIX_ENV=test mix coveralls.post \
   --sha="$TRAVIS_COMMIT" \
   --committer="$(git log -1 $TRAVIS_COMMIT --pretty=format:'%cN')" \
